6591bd8afebb34135d7112b8b0b5316263b41aba
SandjayRJ28 <62304094+SandjayRJ28@users.noreply.github.com>
2022-09-27 12:52:07 +0200

    Add files via upload
    Manifest files

---
+++ awsweb.pp
@@ -0,0 +1,38 @@
+ec2_instance { 'webserverlin':
+ ensure => present,
+ region => 'eu-central-1',
+ availability_zone => 'eu-central-1a',
+ image_id => 'ami-0add4790ba5940e85',
+ instance_type => 't2.micro',
+ monitoring => false,
+ key_name => 'web-vpc',
+ security_groups => ['secnat' , 'pinggroup' , 'secsg-ssh'],
+ subnet => 'webvpc-pub-1a',
+ associate_public_ip_address=> true,
+}
+
+ec2_instance { 'webserverlin2':
+ ensure => present,
+ region => 'eu-central-1',
+ availability_zone => 'eu-central-1a',
+ image_id => 'ami-0add4790ba5940e85',
+ instance_type => 't2.micro',
+ monitoring => false,
+ key_name => 'web-vpc',
+ security_groups => ['secnat' , 'pinggroup' , 'secsg-ssh'],
+ subnet => 'webvpc-pub-1a',
+ associate_public_ip_address=> true,
+}
+
+ec2_instance { 'webserverlin3':
+ ensure => present,
+ region => 'eu-central-1',
+ availability_zone => 'eu-central-1a',
+ image_id => 'ami-0add4790ba5940e85',
+ instance_type => 't2.micro',
+ monitoring => false,
+ key_name => 'web-vpc',
+ security_groups => ['secnat' , 'pinggroup' , 'secsg-ssh'],
+ subnet => 'webvpc-pub-1a',
+ associate_public_ip_address=> true,
+}
